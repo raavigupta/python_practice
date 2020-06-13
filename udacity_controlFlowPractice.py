2a4caf114af9e1958aebd4b71f351a0c95bd07ee
raavigupta <raavigupta@users.noreply.github.com>
2020-06-13 20:56:43 +0530

    Update udacity_controlFlowPractice.py

--- udacity_controlFlowPractice.py
+++ udacity_controlFlowPractice.py
@@ -1,3 +1,8 @@
+# Important point to remember
+# We use <= instead of the < operator, since it was stated that the upper bound is inclusive. 
+# Notice that in each condition, we check if points is in a prize bracket by checking if points is 
+# less than or equal to the upper bound; 
+# we didn't have to check if it was greater than the lower bound. Let's see why this is the case.
 
 
 points = 1  # use this input to make your submission
@@ -31,4 +36,17 @@ elif(points>=151 and points<=180):
     result='Congratulations! You won a {}'.format('penguin!')
 else:
     result='Oh dear, no prize this time.'
+print(result)
+
+points = 174
+
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    result = "Congratulations! You won a penguin!"
+
 print(result)
