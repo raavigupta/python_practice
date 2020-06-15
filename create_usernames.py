ce85863f17332fc2378695c45bdbefbbd18aa8be
raavigupta <raavigupta@users.noreply.github.com>
2020-06-16 04:12:45 +0530

    Update create_usernames.py

--- create_usernames.py
+++ create_usernames.py
@@ -11,3 +11,18 @@ for index in range(len(names)):
 #     usernames=usernames.append(names)
 print(usernames)
 
+
+# My code : Another approach without using range
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+usernames = []
+# My code
+# write your for loop here
+for name in names:
+    name=name.lower().replace(" ","_")
+    usernames.append(name)
+#     usernames=name.append()
+#     names=names.replace(" ","_")
+#     usernames=usernames.append(names)
+print(usernames)
+
+# Output : ['joey_tribbiani', 'monica_geller', 'chandler_bing', 'phoebe_buffay']
