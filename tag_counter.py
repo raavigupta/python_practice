20a2737d8f9849ceb3cb30044c7c5f245a927b19
raavigupta <raavigupta@users.noreply.github.com>
2020-06-16 05:56:06 +0530

    provided solution updated.

--- tag_counter.py
+++ tag_counter.py
@@ -14,3 +14,17 @@ for token in tokens:
     if(token.startswith("<") and token.endswith(">")):
         count = count + 1
 print(count)
+
+
+#Provided solution 
+
+tokens = ['<greeting>', 'Hello World!', '</greeting>']
+
+count = 0
+for token in tokens:
+    if token[0] == '<' and token[-1] == '>':
+        count += 1
+
+print(count)
+
+#Output: 2
